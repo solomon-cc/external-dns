f12df1642d79c17e9d808e556ab232b0e12127b3
chencheng <chencheng@g7.com.cn>
2020-11-17 17:58:56 +0800

    init tencent struck

--- pkg/providers/tencent/tencent.go
+++ pkg/providers/tencent/tencent.go
@@ -1,6 +1,12 @@
 package tencent
 
-import "github.com/sirupsen/logrus"
+import (
+	"sync"
+
+	"external-dns/pkg/types/tencent"
+
+	"github.com/sirupsen/logrus"
+)
 
 // ProviderName is the name of this provider.
 const ProviderName = "tencent"
@@ -8,12 +14,19 @@ const ProviderName = "tencent"
 type checkFun func() error
 
 type Tencent struct {
+	tencent.Options `json:",inline"`
 
-
+	m      *sync.Map
 	logger *logrus.Logger
+
 }
 
 
 func (p *Tencent) GetProviderName() string {
 	return ProviderName
+}
+
+func (p *Tencent) generateClientSDK() error {
+
+	return nil
 }
--- pkg/types/tencent/tencent.go
+++ pkg/types/tencent/tencent.go
@@ -1,1 +1,13 @@
 package tencent
+
+type Options struct {
+	AccessKey    string `json:"access-key,omitempty" yaml:"access-key,omitempty"`
+	AccessSecret string `json:"access-secret,omitempty" yaml:"access-secret,omitempty"`
+	Action       string `json:"action,omitempty" yaml:"action,omitempty"`
+	Domain       string `json:"domain,omitempty" yaml:"domain,omitempty"`
+	Nonce        string `json:"nonce,omitempty" yaml:"nonce,omitempty"`
+	Offset       string `json:"offset,omitempty" yaml:"offset,omitempty"`
+	SubDomain    string `json:"sub-domain,omitempty" yaml:"sub-domain,omitempty"`
+	Timestamp    string `json:"timestamp,omitempty" yaml:"timestamp,omitempty"`
+	Value        string `json:"value,omitempty" yaml:"value,omitempty"`
+}
